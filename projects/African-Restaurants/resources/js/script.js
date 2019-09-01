c463972442ecdef5732372e88844aa728a8cffd1
Jean Paul Mulela <jpbayibansa73@gmail.com>
2019-09-01 17:01:06 -0500

    My main portfolio

---
+++ projects/African-Restaurants/resources/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+                         
+    /* for the sticky navigation*/
+$('.js--section-features').waypoint(function(direction) {
+    if (direction=="dowm") {
+        $('nav').addClass('sticky');
+    } else {
+        $('nav').removeclass('sticky');
+    }
+  }, {
+    offset: '60px;'
+});
+
+
+
+   /* Scroll on buttons */
+   $('.js--scroll-to-plans').click(function () {
+     $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+   }); 
+    
+    $('.js--scroll-to-start').click(function () {
+      $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+   });
+    
+    /* Navigation scroll */
+    
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;  
+          }   
+        }
+      });
+});
+
+  /* Animation on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+        
+    })
+
+});
